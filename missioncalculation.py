dcd0bbed774a9f49a16fc29cd98b4679f4a1f812
vilamarcc <vilamarcc@gmail.com>
2020-03-04 14:17:31 +0100

    Mission calculator adjusted to new perimeter class

--- missioncalculation.py
+++ missioncalculation.py
@@ -3,13 +3,13 @@ import math
 from routes import *
 
 
-def writeSimpleHelixMission(hmax, sep, nWPperCircle, bufferD, cc1, cc2, cc3, cc4, filename):
+def writeSimpleHelixMission(hmin, sep, nWPperCircle, bufferD, perimeter, filename):
     filename = filename + ".txt"
     file = open(str(filename), "w")
     file.write("QGC WPL 110\n")
     alpha = (2*np.pi)/nWPperCircle #rad
-    x,y,z,theta = getHelix(hmax,sep,bufferD,cc1,cc2,cc3,cc4)
-    C = getCenterofPerimeter(cc1,cc2,cc3,cc4)
+    x,y,z,theta = getHelix(sep, bufferD, perimeter, hmin)
+    C = perimeter.C
     xf,yf,zf = getHelixinCoords(x,y,z,C)
     file.write("0 1 0 16 0 0 0 0 " + str(xf[0]) + " " + str(yf[0]) + " " + str(round(zf[0],2)) + " " + "1\n")
     i = 0
@@ -24,10 +24,10 @@ def writeSimpleHelixMission(hmax, sep, nWPperCircle, bufferD, cc1, cc2, cc3, cc4
         j = j + 1
 
     #Añadimos el perimetro para comprobar (eliminar luego)
-    file.write(str(i) +  " 0 10 16 0 0 0 0 " + str(cc1[0]) + " " + str(cc1[1]) + " " + str(0) + " " + "1\n")
-    file.write(str(i + 1) +  " 0 10 16 0 0 0 0 " + str(cc2[0]) + " " + str(cc2[1]) + " " + str(0) + " " + "1\n")
-    file.write(str(i + 2) +  " 0 10 16 0 0 0 0 " + str(cc3[0]) + " " + str(cc3[1]) + " " + str(0) + " " + "1\n")
-    file.write(str(i + 3) +  " 0 10 16 0 0 0 0 " + str(cc4[0]) + " " + str(cc4[1]) + " " + str(0) + " " + "1\n")
+    file.write(str(i) +  " 0 10 16 0 0 0 0 " + str(perimeter.c1[0]) + " " + str(perimeter.c1[1]) + " " + str(0) + " " + "1\n")
+    file.write(str(i + 1) +  " 0 10 16 0 0 0 0 " + str(perimeter.c2[0]) + " " + str(perimeter.c2[1]) + " " + str(0) + " " + "1\n")
+    file.write(str(i + 2) +  " 0 10 16 0 0 0 0 " + str(perimeter.c3[0]) + " " + str(perimeter.c3[1]) + " " + str(0) + " " + "1\n")
+    file.write(str(i + 3) +  " 0 10 16 0 0 0 0 " + str(perimeter.c4[0]) + " " + str(perimeter.c4[1]) + " " + str(0) + " " + "1\n")
     file.close()
 
 def writeSimpleFacadeMission(hmax, sep, n, bufferD, cc1, cc2, filename):
