af4ee30e7315ae4396c9bf0eaf7bd5a5fe6f4ea0
vilamarcc <vilamarcc@gmail.com>
2020-03-04 12:44:21 +0100

    Simple Helix mission properly works now

--- missioncalculation.py
+++ missioncalculation.py
@@ -3,17 +3,25 @@ import math
 from routes import *
 
 
-def writeSimpleHelixMission(hmax, sep, nCircles, nWP, bufferD, cc1, cc2, cc3, cc4):
-    file = open("Mission1.txt", "w")
+def writeSimpleHelixMission(hmax, sep, nWPperCircle, bufferD, cc1, cc2, cc3, cc4, filename):
+    filename = filename + ".txt"
+    file = open(str(filename), "w")
     file.write("QGC WPL 110\n")
-    x,y,z = getHelix(hmax,sep,bufferD,cc1,cc2,cc3,cc4)
+    alpha = (2*np.pi)/nWPperCircle #rad
+    x,y,z,theta = getHelix(hmax,sep,bufferD,cc1,cc2,cc3,cc4)
     C = getCenterofPerimeter(cc1,cc2,cc3,cc4)
     xf,yf,zf = getHelixinCoords(x,y,z,C)
     file.write("0 1 0 16 0 0 0 0 " + str(xf[0]) + " " + str(yf[0]) + " " + str(round(zf[0],2)) + " " + "1\n")
     i = 0
-    while (i < len(xf)):
-        file.write(str(i + 1) +  " 0 10 82 0 0 0 0 " + str(xf[i]) + " " + str(yf[i]) + " " + str(round(zf[i],2)) + " " + "1\n")
-        i = i + 1
+    j = 0
+    rpm = 0
+    while (j < len(xf)):
+        if(round(alpha*i,1) == round(theta[j],1) or round(alpha*i,1) == round(theta[j] - (2*np.pi*rpm),1) or round(alpha*i,1) + 0.1 == round(theta[j],1) or round(alpha*i,1) - 0.1 == round(theta[j],1)):
+            file.write(str(i + 1) +  " 0 10 82 0 0 0 0 " + str(xf[j]) + " " + str(yf[j]) + " " + str(round(zf[j],2)) + " " + "1\n")
+            i = i + 1
+        if(round(theta[j] - (2*np.pi*(rpm + 1))) == 0):
+            rpm = rpm + 1
+        j = j + 1
 
     #Añadimos el perimetro para comprobar (eliminar luego)
     file.write(str(i) +  " 0 10 16 0 0 0 0 " + str(cc1[0]) + " " + str(cc1[1]) + " " + str(0) + " " + "1\n")
@@ -21,3 +29,8 @@ def writeSimpleHelixMission(hmax, sep, nCircles, nWP, bufferD, cc1, cc2, cc3, cc
     file.write(str(i + 2) +  " 0 10 16 0 0 0 0 " + str(cc3[0]) + " " + str(cc3[1]) + " " + str(0) + " " + "1\n")
     file.write(str(i + 3) +  " 0 10 16 0 0 0 0 " + str(cc4[0]) + " " + str(cc4[1]) + " " + str(0) + " " + "1\n")
     file.close()
+
+def writeSimpleFacadeMission(hmax, sep, n, bufferD, cc1, cc2, filename):
+    filename = filename + ".txt"
+    file = open(filename, "w")
+    file.write("QGC WPL 110\n")
