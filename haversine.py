cfa2cba2dd2de2e70b33857df4a5c16acbb8516d
vilamarcc <vilamarcc@gmail.com>
2020-03-30 19:21:56 +0200

    Fixes

--- haversine.py
+++ haversine.py
@@ -18,9 +18,9 @@ def getLocationAtBearing(lat1, lon1, distance, angle):
     lat1 = math.radians(lat1) # Current lat point converted to radians
     lon1 = math.radians(lon1) # Current long point converted to radians
     lat2 = math.degrees(math.asin(math.sin(lat1)*math.cos(distance/R) + math.cos(lat1)*math.sin(distance/R)*math.cos(angle)))
-    lat2 = round(lat2, 7)
+    lat2 = lat2
     lon2 = math.degrees(lon1 + math.atan2(math.sin(angle)*math.sin(distance/R)*math.cos(lat1), math.cos(distance/R) - math.sin(lat1)*math.sin(lat2)))
-    lon2 = round(lon2, 7)
+    lon2 = lon2
     return lat2, lon2
 
 def getBearingBetweenCoordinates(lat1, lon1, lat2, lon2):
